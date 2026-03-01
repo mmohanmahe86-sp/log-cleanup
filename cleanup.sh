195aa4fa2f8465586847d232e1b48d87f8a2caa7
mmohanmahe86 <mmohanmahe86@gmail.com>
2026-03-01 08:07:02 +0000

    Initial log cleanup automation script

---
+++ cleanup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LOG_DIR="/var/log"
+DAYS=7
+
+echo "===== Log Cleanup Started ====="
+date
+
+echo "Removing logs older than $DAYS days..."
+
+find $LOG_DIR -type f -name "*.log" -mtime +$DAYS -exec rm -f {} \;
+
+echo "Cleanup completed"
+date
